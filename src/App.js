4ca05c0df0d470e8f369185fdcb57c4efa377850
slay <palgalow@gmail.com>
2024-08-02 22:54:29 +0900

    [fix] 로그인 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div className='app-container'>
+      <h2 className='todo-title'>TO DO 집안일</h2>
+      <div className='login-container'>
+        <div className='login-box'>
+          <h1 className='login-title'>USER LOGIN</h1>
+          <p className='login-subtitle'>집안일 트래커</p>
+          <div className='login-avatar'></div>
+          <form className='login-form'>
+            <input type='text' placeholder='username' className='login-input'/>
+            <input type='password' placeholder='password' className='login-input'/>
+            <div className='login-remember-me'>
+              <input type='checkbox' id='remember-me'/>
+              <label htmlFor='remeber-me'>remember me</label>
+            </div>
+            <button type='submit' className='login-button'>LOGIN</button>
+          </form>
+          <a href='/forgot-password' className='login-forgot-password'>Forgot password?</a>
+        </div>
+      </div>
     </div>
   );
 }
