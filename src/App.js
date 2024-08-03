c443bb8af22174f491c1a8e8f40d497b582e2016
slay <palgalow@gmail.com>
2024-08-03 22:54:52 +0900

    Feat: 회원가입 창 구현
    회원가입 js와 css 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import TodoPage from './pages/TodoPage';
+import SignupPage from './pages/SignupPage';
 import './App.css';
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
                 <Routes>
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/todos" element={<TodoPage />} />
+                    <Route path="/signup" element={<SignupPage/>} />
+                    <Route path="/" element={<LoginPage/>}/>
                 </Routes>
             </div>
         </Router>
---
+++ src/pages/SignupPage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './SignupPage.css';
+
+function SignupPage() {
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // 회원가입 로직 추가
+        navigate('/login');
+    };
+
+    return (
+        <div className='app-container'>
+            <h2 className='todo-title'>TO DO 집안일</h2>
+            <div className='signup-container'>
+                <div className='signup-box'>
+                    <h1 className='signup-title'>USER SIGNUP</h1>
+                    <p className='signup-subtitle'>집안일 트래커</p>
+                    <form className='signup-form' onSubmit={handleSubmit}>
+                        <input type='text' placeholder='username' className='signup-input' />
+                        <input type='email' placeholder='email' className='signup-input' />
+                        <input type='password' placeholder='password' className='signup-input' />
+                        <input type='password' placeholder='confirm password' className='signup-input' />
+                        <button type='submit' className='signup-submit'>회원가입</button>
+                    </form>
+                    <a href='/login' className='signup-login-link'>이미 계정이 있으신가요? 로그인</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SignupPage;
